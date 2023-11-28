3109a58c6deceb33751060c47382fb6c08173b69
Ato Toffah <atotoffah@gmail.com>
2023-11-28 10:17:00 +0000

    Extra: Fix 1

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,38 +5,40 @@ package { 'nginx':
 }
 
 file { 'index.html':
-  path => '/var/www/html/',
+  path    => '/var/www/html/',
   content => 'Hello World!',
 }
 
 file { 'error-page.html':
-  path => '/var/www/html/',
+  path    => '/var/www/html/',
   content => "Ceci n'est pas une page\n",
 }
 
-$server_config = @("CONFIG")
-server {
+file { 'default':
+  path    => '/etc/nginx/sites-enabled',
+  content =>
+"server {
         listen 80 default_server;
         listen [::]:80 default_server;
-        root /var/www/html;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
         index index.html index.htm index.nginx-debian.html;
         server_name _;
         location / {
-                try_files $uri $uri/ =404;
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
         }
         error_page 404 /error-page.html;
-        location = /error-page.html {
-                internal;
+        location  /error-page.html {
+            internal;
         }
-        if ($request_filename ~ redirect_me){
-                rewrite ^ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;
         }
 }
-| CONFIG	
-
-file { 'default':
-  path => '/etc/nginx/sites-enabled',
-  content => $server_config
+"
 }
 
 
