4f299bbcd5dde7c2540d039627af7354348b96b1
Ato Toffah <atotoffah@gmail.com>
2024-01-02 19:45:34 +0000

    0. Gather data from an API:Fix 4

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     todos = requests.get(base_url + todos_endpoint).json()
     done = [x for x in todos if x['completed'] is True]
     print(f"Employee {user['name']} " +
-          "is done with tasks({len(done)}/{len(todos)}):")
+          f"is done with tasks({len(done)}/{len(todos)}):")
     for todo in done:
         print(f"\t {todo['title']}")
