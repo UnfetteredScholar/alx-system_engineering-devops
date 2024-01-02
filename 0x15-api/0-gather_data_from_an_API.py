e8810de87be8f8cb814f15e2eefd7b2f8a62895b
Ato Toffah <atotoffah@gmail.com>
2024-01-02 19:38:55 +0000

    0. Gather data from an API:Fix 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""For a given employee ID,
-returns information about his/her TODO list progress."""
-import requests
-from sys import argv
+"""
+For a given employee ID,
+returns information about his/her TODO list progress.
+"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
 
-base_url = 'https://jsonplaceholder.typicode.com'
-user_endpoint = f'/users/{argv[1]}'
-todos_endpoint = f'/users/{argv[1]}/todos'
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_endpoint = f'/users/{argv[1]}'
+    todos_endpoint = f'/users/{argv[1]}/todos'
 
-user = requests.get(base_url + user_endpoint).json()
-todos = requests.get(base_url + todos_endpoint).json()
-done = [x for x in todos if x['completed'] is True]
-for todo in done:
-    print(f"\t {todo['title']}")
+    user = requests.get(base_url + user_endpoint).json()
+    todos = requests.get(base_url + todos_endpoint).json()
+    done = [x for x in todos if x['completed'] is True]
+    for todo in done:
+        print(f"\t {todo['title']}")
