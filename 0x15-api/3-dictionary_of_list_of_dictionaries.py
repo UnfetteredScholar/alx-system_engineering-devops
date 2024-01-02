7f60d96d8e5cf19673282c99c19d91308bf7bcb3
Ato Toffah <atotoffah@gmail.com>
2024-01-02 20:30:24 +0000

    3. Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Records all tasks from all employeesand
+export to json
+"""
+if __name__ == '__main__':
+    import json
+    import requests
+
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_endpoint = f'/users'
+    employees = {}
+
+    users = requests.get(base_url + user_endpoint).json()
+
+    for user in users:
+        todos_endpoint = f'/users/{user["id"]}/todos'
+        todos = requests.get(base_url + todos_endpoint).json()
+        tasks = []
+
+        for todo in todos:
+            task = {
+                'username': user['username'],
+                'task': todo['title'],
+                'completed': todo['completed']
+            }
+            tasks.append(task)
+        employees[f'{user["id"]}'] = tasks
+    with open(f"todo_all_employees.json", 'w') as f:
+        f.write(json.dumps(employees))
