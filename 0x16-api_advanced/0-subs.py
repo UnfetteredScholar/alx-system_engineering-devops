45e16b03c2cef0dfd847a0d3b7e47a90a64572cf
Ato Toffah <atotoffah@gmail.com>
2024-01-17 13:39:41 +0000

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a function for retrieving
+number of subreddit subscribers"""
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """
+    Gets the number of subscribers to
+    a subreddit.
+
+    Args:
+        subreddit (str): the name of the subreddit
+
+    Returns:
+        The number of subs else 0 for invalid name.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    try:
+        response = requests.get(
+            url, headers={"User-Agent": "My-User-Agent"}, allow_redirects=False
+        )
+        data = response.json()
+        return data["data"]["subscribers"]
+    except Exception as ex:
+        return 0
